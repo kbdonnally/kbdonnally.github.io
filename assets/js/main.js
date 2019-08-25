d9305ee5bbe482e7402d8cab65fa4b4a6f74351c
Katherine Donnally <kbd3ax@virginia.edu>
2019-08-25 16:26:40 -0400

    Add click to smooth scroll down to response text for IN THAT CASE text

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+// portfolio
 if (document.querySelector('.portfolio-wrapper')) {
 	const gallery = document.querySelector('.portfolio__gallery');
 	const button  = document.querySelector('.portfolio-modal__button');
@@ -31,5 +32,22 @@ if (document.querySelector('.portfolio-wrapper')) {
 
 	function hideModal() {
 		modal.classList.remove('portfolio-modal--show');
+	}
+}
+
+// home
+if (document.querySelector('.new-home-wrapper')) {
+	const opt1 		 = document.querySelector('.nh-options__option-1');
+	const opt2 		 = document.querySelector('.nh-options__option-2');
+	const opt3 		 = document.querySelector('.nh-options__option-3');
+	let inThatCase	 = document.querySelector('.nh-p3__title');
+	let responseElem = document.querySelector('.nh-p3__text');
+
+	opt1.addEventListener('click', scrollDown);
+	opt2.addEventListener('click', scrollDown);
+	opt3.addEventListener('click', scrollDown);
+
+	function scrollDown() {
+		inThatCase.scrollIntoView({behavior: 'smooth'});
 	}
 }
